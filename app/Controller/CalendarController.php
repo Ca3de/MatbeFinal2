ce54500de95bc91f8eaedcc22224496c947cdf6e
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:12:01 -0500

    Update CalendarController.php

--- app/Controller/CalendarController.php
+++ app/Controller/CalendarController.php
@@ -1,1 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Controller;
+
+class CalendarController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = "Church Calendar";
+        $this->renderView('calendar.php', compact('pageTitle'));
+    }
+}
 
