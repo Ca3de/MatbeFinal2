aa70d1d8d1d6edd6d1677ae373223d7c77a59af3
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:06:31 -0500

    Update Controller.php

--- core  /Controller.php
+++ core  /Controller.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace Core;
+
+class Controller
+{
+    /**
+     * Renders a view within the main layout.
+     */
+    protected function renderView(string $viewPath, array $data = [])
+    {
+        // Extract data for the view
+        extract($data);
+
+        // The $viewPath is something like 'home.php', 'about.php', etc.
+        include __DIR__ . '/../app/Views/layout/main.php';
+    }
+}
