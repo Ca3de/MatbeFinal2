05ba51b94f5502a39f966d021dcd0a6c8fdae0e2
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:13:41 -0500

    Update gallery.php

--- app/Views/gallery.php
+++ app/Views/gallery.php
@@ -1,1 +1,16 @@
+<h2>Church Gallery</h2>
+<div class="row">
+  <?php foreach($images as $image): ?>
+    <div class="col-md-4 mb-3">
+      <div class="card">
+        <img src="<?= $base_url ?>/uploads/<?= htmlspecialchars($image->filename) ?>" 
+             class="card-img-top" 
+             alt="<?= htmlspecialchars($image->title) ?>">
+        <div class="card-body">
+          <h5 class="card-title"><?= htmlspecialchars($image->title) ?></h5>
+        </div>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
 
