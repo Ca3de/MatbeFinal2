4bdd112963446e6d59ec3432661dbc09262f2311
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:13:28 -0500

    Update sermons.php

--- app/Views/sermons.php
+++ app/Views/sermons.php
@@ -1,1 +1,19 @@
+<h2 class="mb-4">Sermons & Messages</h2>
+<div class="row">
+  <?php foreach($sermons as $sermon): ?>
+    <div class="col-md-4 mb-3">
+      <div class="card">
+        <iframe 
+          class="card-img-top"
+          height="200"
+          src="https://www.youtube.com/embed/<?= htmlspecialchars($sermon->youtube_video_id) ?>"
+          allowfullscreen>
+        </iframe>
+        <div class="card-body">
+          <h5 class="card-title"><?= htmlspecialchars($sermon->title) ?></h5>
+        </div>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
 
