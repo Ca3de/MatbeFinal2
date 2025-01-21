d3dce7b28a1598f2385b7f6e1d72294647be473c
Israel Oladeji <chengadc@gmail.com>
2025-01-20 22:42:38 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,17 @@
+<?php
 
+return [
+    'db' => [
+        'host' => 'sql.freedb.tech',
+        'database' => 'freedb_church_db',
+        'user' => 'YOUR_DB_USERNAME',
+        'pass' => 'YOUR_DB_PASSWORD',
+        'port' => 3306
+    ],
+    'youtube' => [
+        'api_key' => 'YOUR_YOUTUBE_API_KEY',
+        'playlist_id' => 'YOUR_PLAYLIST_ID'
+    ],
+    // Adjust this base URL as needed, e.g. for local dev:
+    'base_url' => 'http://localhost/churchweb/public'
+];
