7ed10493328839272f572c18ed7856af4042950c
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:07:37 -0500

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace App\Models;
+
+use Core\Database;
+use PDO;
+
+class Image
+{
+    public $id;
+    public $filename;
+    public $title;
+    public $created_at;
+
+    public static function all()
+    {
+        $config = require __DIR__ . '/../../../config/config.php';
+        $db = Database::getInstance($config['db'])->getConnection();
+
+        $stmt = $db->query("SELECT * FROM images ORDER BY created_at DESC");
+        return $stmt->fetchAll(PDO::FETCH_CLASS, self::class);
+    }
+
+    public function save()
+    {
+        $config = require __DIR__ . '/../../../config/config.php';
+        $db = Database::getInstance($config['db'])->getConnection();
+
+        $stmt = $db->prepare("INSERT INTO images (filename, title) VALUES (:filename, :title)");
+        $stmt->execute([
+            ':filename' => $this->filename,
+            ':title' => $this->title
+        ]);
+    }
+}
