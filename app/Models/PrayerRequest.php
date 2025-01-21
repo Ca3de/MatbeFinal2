c100dd6499cabb523f8f076afcdc0107782afae9
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:08:26 -0500

    Update PrayerRequest.php

--- app/Models/PrayerRequest.php
+++ app/Models/PrayerRequest.php
@@ -1,1 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Core\Database;
+use PDO;
+
+class PrayerRequest
+{
+    public $id;
+    public $name;
+    public $email;
+    public $request;
+    public $created_at;
+
+    public function save()
+    {
+        $config = require __DIR__ . '/../../../config/config.php';
+        $db = Database::getInstance($config['db'])->getConnection();
+
+        $stmt = $db->prepare("INSERT INTO prayer_requests (name, email, request) 
+                              VALUES (:name, :email, :request)");
+        $stmt->execute([
+            ':name' => $this->name,
+            ':email' => $this->email,
+            ':request' => $this->request
+        ]);
+    }
+}
 
