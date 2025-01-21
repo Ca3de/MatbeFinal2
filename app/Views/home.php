f978aa1c8826fddf5be61c15c67c6522826d546b
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:13:18 -0500

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,1 +1,6 @@
+<div class="bg-light p-5 rounded-3 mb-4">
+  <h1 class="display-4">Welcome to Our Church</h1>
+  <p class="lead">Join us for worship, community, and spiritual growth.</p>
+  <a href="<?= $base_url ?>/about" class="btn btn-primary btn-lg">Learn More</a>
+</div>
 
