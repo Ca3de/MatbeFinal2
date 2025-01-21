cdf14453528a20c740e42ba71e1c7295e1db60ca
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:05:50 -0500

    Update Database.php

--- core  /Database.php
+++ core  /Database.php
@@ -1,1 +1,36 @@
+<?php
 
+namespace Core;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct(array $dbConfig)
+    {
+        try {
+            $dsn = "mysql:host={$dbConfig['host']};port={$dbConfig['port']};dbname={$dbConfig['database']};charset=utf8mb4";
+            $this->pdo = new PDO($dsn, $dbConfig['user'], $dbConfig['pass']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("DB Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance(array $dbConfig)
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($dbConfig);
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+}
