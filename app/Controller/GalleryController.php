9c1eeee1251d854dc5c7dc8c974b493f3294844d
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:12:15 -0500

    Update GalleryController.php

--- app/Controller/GalleryController.php
+++ app/Controller/GalleryController.php
@@ -1,1 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Controller;
+use App\Models\Image;
+
+class GalleryController extends Controller
+{
+    public function index()
+    {
+        $images = Image::all();
+        $pageTitle = "Church Gallery";
+        $this->renderView('gallery.php', compact('images', 'pageTitle'));
+    }
+}
 
