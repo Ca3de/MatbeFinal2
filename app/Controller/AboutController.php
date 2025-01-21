60b8a7e499eca803fa3eecac558ab31dffe876e7
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:11:38 -0500

    Update AboutController.php

--- app/Controller/AboutController.php
+++ app/Controller/AboutController.php
@@ -1,1 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Controller;
+
+class AboutController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = "About Our Church";
+        $this->renderView('about.php', compact('pageTitle'));
+    }
+}
 
