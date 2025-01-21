edc19f5aa0e0fac06c836c692d1baef948a71b0c
Israel Oladeji <chengadc@gmail.com>
2025-01-20 23:11:26 -0500

    Update HomeController.php

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,1 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = "Welcome to Our Church";
+        $this->renderView('home.php', compact('pageTitle'));
+    }
+}
 
